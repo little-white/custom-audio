c43ccfab770cee54ec548e0bb21ee27f164a02b8
guoyaqi <1664216915@qq.com>
2016-12-19 00:03:09 +0800

    play and pause by js

---
+++ js/index.js
@@ -0,0 +1,6 @@
+document.getElementById('play').onclick = function() {
+    document.getElementById('audio').play();
+}
+document.getElementById('pause').onclick = function() {
+    document.getElementById('audio').pause();
+}
