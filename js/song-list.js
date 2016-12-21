9928c9dffbe332d89153d895c485dc9d8700eff9
guoyaqi <1664216915@qq.com>
2016-12-21 10:59:07 +0800

    store mp3 file to local

--- js/song-list.js
+++ js/song-list.js
@@ -1,6 +1,6 @@
 var songList = [{
 	name: '简单爱',
-	url: 'http://yinyueshiting.baidu.com/data2/music/41526971/107364441482058861128.mp3?xcode=f5d02321a9bb979a8c3c42cb27c397d9',
+	url: '../audio/jiandanai.mp3',
 	lrcLink: 'http://musicdata.baidu.com/data2/lrc/13822026/13822026.lrc?_=1482151911775'
 
 },{
